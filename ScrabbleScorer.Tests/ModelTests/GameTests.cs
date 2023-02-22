15d1cc21b6efa2f9020be703997735618f937fcd
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-22 10:35:51 -0800

    refactor tests

--- ScrabbleScorer.Tests/ModelTests/GameTests.cs
+++ ScrabbleScorer.Tests/ModelTests/GameTests.cs
@@ -13,17 +13,15 @@ namespace ScrabbleScorer.Tests
     public void LetterScore_ReturnsScoreForSingleLetter_Int()
     {
       char input = 'a';
-      ScrabbleScore firstScore = new ScrabbleScore();
-      int result = firstScore.LetterScore(input);
+      int result = ScrabbleScore.LetterScore(input);
       Assert.AreEqual(1, result);
-    } //code that passes switch case
+    } //code that passes switch case -- test was re
 
     [TestMethod]
     public void WordScore_ReturnsScoreForEntireWord_Int()
     {
       string input = "hi";
-      ScrabbleScore firstWord = new ScrabbleScore();
-      int result = firstWord.WordScore(input);
+      int result = ScrabbleScore.WordScore(input);
       Assert.AreEqual(5,result);
     } //testing return of score for string. test that runs through switch case every instance of letter appearing in word and adds them
   }
